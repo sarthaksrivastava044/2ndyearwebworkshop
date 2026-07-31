42f026cabed4c8f85a95053bfefb25a62cdde08d
sarthaksrivastava044 <sarthaksirvastava044@gmail.com>
2026-07-31 11:45:43 +0530

    commit

--- DAY2/a.js
+++ DAY2/a.js
@@ -1,4 +1,51 @@
 // variables
-console.log("==variables==")
-var text="something";
-console.log(text);
+// console.log("===variables==")
+// var first="Sarthak";
+// var last="Srivastava"
+// console.log(first);
+// console.log(last);
+//if else
+// var a=40;
+// var b=30;
+// if(a>b){
+//     console.log("a is greater" );
+// }
+// else{
+//     console.log("b is greater");
+// }
+
+//function
+// function abes(){
+//     console.log("welcome students")
+//     console.log("welcome to abes")
+// }
+
+// abes();
+
+// function greet(first,last){
+//     console.log("Hello"," ",first," ",last)
+// }
+// greet("Sachin" , "Tyagi");
+
+// function add(x,y){
+//     return x+y;
+// }
+// let result=add(10,20);
+// console.log("sum is ",result);
+// function fullName(fname="ABES",lname=" Engineering College"){
+//     return fname+lname;
+// }
+// console.log(fullName())
+// console.log(fullName("Sarthak"," ","Srivastava"));
+
+// function totalMarks(math ,english,science){
+//     return math+english+science;
+// }
+// function percentage(total){
+//     return total/300 *100;
+// }
+// var total=totalMarks(80,90,89);
+
+
+
+
