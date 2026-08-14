d006d4bccaace3e1b6641b5be5cd1a2b66f7c2be
sarthaksrivastava044 <sarthaksirvastava044@gmail.com>
2026-08-14 12:06:24 +0530

    day3

--- DAY3/one.js
+++ DAY3/one.js
@@ -16,8 +16,36 @@
 // }
 // check(12)
 //Calculate Percentage prog
-function calculatePercentage(maths,eng,chem){
-    let total=maths+eng+chem
-    return total*100/300
-}
-console.log(calculatePercentage(80,75,90),"%")
+// function calculatePercentage(maths,eng,chem){
+//     let total=maths+eng+chem
+//     return total*100/300
+// }
+// console.log(calculatePercentage(80,75,90),"%")
+// console.log("=== Sum of Array Elements ===");
+
+// function sumOfArray(arr){
+//     var sum = 0;
+//     for(var i=0;i<arr.length;i++){
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+
+// var numbers = [1, 2, 3, 4, 5];
+// console.log("Sum of array elements is: " + sumOfArray(numbers));
+// let arr=[['Tanishque','Tyagi',19],['Sarthak','Srivastava',20],['Rahul','Sing',18]]
+// for(i of arr){
+//     console.log("firstname",i[0])
+//         console.log("lastname",i[1])
+//     console.log("age",i[2])
+// }
+
+
+
+
+
+
+
+
+
+
