307cfcb31a16d990c32a0d76aeda8df8e9f9c9d8
sarthaksrivastava044 <sarthaksirvastava044@gmail.com>
2026-09-21 15:44:45 +0530

    day8

--- DAY8/day8.js
+++ DAY8/day8.js
@@ -1,14 +1,72 @@
-class student{
-    constructor(){
-        console.log("Hello")
+// class student{
+//     constructor(){
+//         console.log("Hello , how are you")
+//     }
+// }
+// class Sarthak extends student{
+//     constructor(){
+//         super()
+//         console.log("Hi")
+//     }
+//     static name=['Sarthak']
+// }
+// console.log(Sarthak.name)
+// let obj=new Sarthak()
+// class Employee{
+//     name="";id=0;basic=0;
+//     constructor(name,id,basic) {
+//         this.name=name;
+//         this.id=id;
+//         this.basic=basic;
+//     }
+//     calculate(){
+//         return this.basic
+//     }
+// }
+// class Manger extends Employee{
+//     add=0
+//     constructor(update,name,id,bsd)
+//     {
+//         super(name,id,bsd)
+//         this.add=update
+//     }
+//     calculate(){
+//         return this.basic+this.add
+//     }
+// }
+// let obj1=new Manger(123,"Sarthak",12,9000)
+// console.log("Update Salary",obj1.calculate())
+class Employee{
+    name="";id=0;basic=0;
+    constructor(name,id,basic) {
+        this.name=name;
+        this.id=id;
+        this.basic=basic;
+    }
+    calculate(){
+        return this.basic
     }
 }
-class Sarthak extends student{
-    constructor(){
-        super()
-        console.log("Hi")
+class Manger extends Employee{
+    add=0
+    constructor(update,name,id,bsd)
+    {
+        super(name,id,bsd)
+        this.add=update
     }
-    static name=['Sarthak']
+    calculate(){
+        return this.basic+this.add
+    }
 }
-console.log(Sarthak.name)
-let obj=new Sarthak()
+let obj=new Manger(123,"Sarthak",12,9000)
+console.log("Update Salary",obj.calculate())
+
+function name(show)
+{
+    console.log("Hello")
+    setTimeout(()=>{console.log("Sarthak")},3000)
+    show()
+}
+name(()=>{
+    console.log("Shivam")
+})
